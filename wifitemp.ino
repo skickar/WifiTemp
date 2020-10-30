7c99b839546ac81beab3fd6389026d3f571c0f30
Skickar <40251293+skickar@users.noreply.github.com>
2020-10-30 10:31:41 -0600

    Update wifitemp.ino
    Fixing, removing bad code

--- wifitemp.ino
+++ wifitemp.ino
@@ -1,89 +1,21 @@
-/* SFE_BMP180 library example sketch
-
-This sketch shows how to use the SFE_BMP180 library to read the
-Bosch BMP180 barometric pressure sensor.
-https://www.sparkfun.com/products/11824
-
-Like most pressure sensors, the BMP180 measures absolute pressure.
-This is the actual ambient pressure seen by the device, which will
-vary with both altitude and weather.
-
-Before taking a pressure reading you must take a temparture reading.
-This is done with startTemperature() and getTemperature().
-The result is in degrees C.
-
-Once you have a temperature reading, you can take a pressure reading.
-This is done with startPressure() and getPressure().
-The result is in millibar (mb) aka hectopascals (hPa).
-
-If you'll be monitoring weather patterns, you will probably want to
-remove the effects of altitude. This will produce readings that can
-be compared to the published pressure readings from other locations.
-To do this, use the sealevel() function. You will need to provide
-the known altitude at which the pressure was measured.
-
-If you want to measure altitude, you will need to know the pressure
-at a baseline altitude. This can be average sealevel pressure, or
-a previous pressure reading at your altitude, in which case
-subsequent altitude readings will be + or - the initial baseline.
-This is done with the altitude() function.
-
-Hardware connections:
-
-- (GND) to GND
-+ (VDD) to 3.3V
-
-(WARNING: do not connect + to 5V or the sensor will be damaged!)
-
-You will also need to connect the I2C pins (SCL and SDA) to your
-Arduino. The pins are different on different Arduinos:
-
-Any Arduino pins labeled:  SDA  SCL
-Uno, Redboard, Pro:        A4   A5
-Mega2560, Due:             20   21
-Leonardo:                   2    3
-
-Leave the IO (VDDIO) pin unconnected. This pin is for connecting
-the BMP180 to systems with lower logic levels such as 1.8V
-
-Have fun! -Your friends at SparkFun.
-
-The SFE_BMP180 library uses floating-point equations developed by the
-Weather Station Data Logger project: http://wmrx00.sourceforge.net/
-
-Our example code uses the "beerware" license. You can do anything
-you like with this code. No really, anything. If you find it useful,
-buy me a beer someday.
-
-V10 Mike Grusin, SparkFun Electronics 10/24/2013
-V1.1.2 Updates for Arduino 1.6.4 5/2015
-*/
-
-// Your sketch must #include this library, and the Wire library.
-// (Wire is a standard library included with Arduino.):
+// BMP180 reference code
+// https://github.com/LowPowerLab/SFE_BMP180/blob/master/examples/SFE_BMP180_example/SFE_BMP180_example.ino
 
 #include <SFE_BMP180.h>
-#include <Wire.h>
-
-// You will need to create an SFE_BMP180 object, here called "pressure":
-
-SFE_BMP180 pressure;
-
-#define ALTITUDE 937.0 // Altitude of SparkFun's HQ in Boulder, CO. in meters
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+ 
+SFE_BMP180 BMP;
+char apHostName[50];
+void resetHostName();
 
-char APname[30];
-
-unsigned long begintime;
-
-void setup()
-{
+//------------------------------------------------------------------------------
+ 
+void setup() {
   Serial.begin(9600);
-  Serial.println("REBOOT");
-
-  // Initialize the sensor (it is important to get calibration values stored on the device).
-  if (pressure.begin())
+  Serial.println("BOOT");
+ 
+  // Initialize the sensor
+  if (BMP.begin())
     Serial.println("BMP180 init success");
   else{
     // Oops, something went wrong, this is usually a connection problem,
@@ -91,39 +23,41 @@ void setup()
     Serial.println("BMP180 init fail\n\n");
     while(1); // Pause forever.
   }
-
+ 
   WiFi.mode(WIFI_AP);
 }
+ 
+//------------------------------------------------------------------------------
 
-void resetHostName();
-
-char apHostName[30];
 
 void loop(){
-  delay(60000);
-  double T;
-  
-  char status = pressure.startTemperature();
+  double celsius;
+ 
+  char status = BMP.startTemperature();
   if (status != 0){
     // Wait for the measurement to complete:
     delay(status);
-
+ 
     // Retrieve the completed temperature measurement:
     // Note that the measurement is stored in the variable T.
     // Function returns 1 if successful, 0 if failure.
-
-    status = pressure.getTemperature(T);
+ 
+    status = BMP.getTemperature(celsius);
     if (status != 0){
-      // make hostname == temperature
-      sprintf(apHostName, "%.2f °C // %.2f °F", T, (9.0/5.0)*T+32.0,2);
+      // convert Celsius to Fahrenheit
+      double fahrenheit = (9.0/5.0)*celsius+32.0;
+ 
+      // set the host name to the temperature
+      sprintf(apHostName, "%.2f °C // %.2f °F", celsius, fahrenheit);
       resetHostName();
     }
   }
+  delay(60000);
 }
 
+//------------------------------------------------------------------------------
 
 void resetHostName(){
-  //WiFi.mode(WIFI_AP);
   Serial.print("Resetting host name to: ");
   Serial.println(apHostName);
   WiFi.softAPdisconnect(true);
